e870befdaa9e86862d356823c398884d725ebc9a
InfroLab <dazran85@gmail.com>
2020-04-07 15:28:52 +0300

    Fixes to get_db_path()

--- InfroBot/utility/path.py
+++ InfroBot/utility/path.py
@@ -3,6 +3,6 @@ import os
 # Returns db path
 def get_db_path():
     dir = os.path.dirname(__file__)
-    path = os.path.join(dir, 'bot.db')
+    path = os.path.join(dir, '..', 'db', 'bot.db')
     
     return path
