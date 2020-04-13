a11bc37e1162e34fedb8e7c38b8c83faa2c84f6f
InfroLab <dazran85@gmail.com>
2020-04-14 01:29:56 +0300

    Fix for !history messages subcommand

--- InfroBot/cogs/history.py
+++ InfroBot/cogs/history.py
@@ -11,10 +11,9 @@ class History(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.has_guild_permissions(administrator=True)
     @commands.command(name='history')
     async def history(self, ctx, arg, query_user=None):
-        if arg == 'messages':
+        if arg == 'messages' and ctx.author.id == ctx.guild.owner_id:
             user = ctx.author if not query_user else query_user
             user_id = user.id
             guild_id = ctx.guild.id
@@ -22,11 +21,13 @@ class History(commands.Cog):
 
             # Dumping json and writing it into file
             dir = os.path.dirname(__file__)
-            history_path = os.path.join(dir, fr'messages_history\{guild_id}_{user_id}.json')
-            with open(history_path, 'r+') as f:
-                json.dump(history_dicts , f, indent=4, separators=(',', ':'))
-                history_file = discord.File(history_path, filename=f'{guild_id}-{user_id}.json', spoiler=True)
-                await ctx.author.send(f'**Статистика сообщений пользователя {user.mention} по серверу {ctx.guild.name}:**', file=history_file)
+            history_path = os.path.join(dir, 'messages_history', f'{guild_id}_{user_id}.json')
+            f = open(history_path, 'w')
+            json.dump(history_dicts , f, indent=4, separators=(',', ':'))
+            history_file = discord.File(history_path, filename=f'{guild_id}-{user_id}.json', spoiler=True)
+            await ctx.author.send(f'**Статистика сообщений пользователя {user.mention} по серверу {ctx.guild.name}:**', file=history_file)
+            f.close()
+    
     @history.error
     async def history_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
